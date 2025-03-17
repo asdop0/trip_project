2f9c6fa430d42b205c4da670b69a0c823732c8bd
apiod <trtxcrop4893@naver.com>
2025-03-17 15:15:35 +0900

    map

---
+++ front/README.md
@@ -0,0 +1,5 @@
+node -v // v22.14.0
+npm -v // v10.9.2
+
+cd front // 프론트 파일 이동후
+npm start // 실행
---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MapComponent from "./googleMap/gMCall";
+
+function App() {
+  return (
+    <div>
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
