10e0b292f188a94335f69944ebc250fdb45686cc
apiod <trtxcrop4893@naver.com>
2025-03-18 16:25:12 +0900

    'signin,signup,api'

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import MapComponent from "./googleMap/gMCall";
+import SignIn from "./login/SignIn";
+import SingUP from "./login/SignUp";
 
 function App() {
-  return (
-    <div>
-      <MapComponent />
-    </div>
-  );
+    return (
+        <div>
+            <SignIn />
+            <SingUP />
+        </div>
+    );
 }
-
+/*
+<MapComponent />
+<SignIn />
+*/
 export default App;
--- front/src/googleMap/gMCall.js
+++ front/src/googleMap/gMCall.js
@@ -2,23 +2,29 @@ import React from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const containerStyle = {
-  width: "100%",
-  height: "500px",
+    width: "100%",
+    height: "500px",
 };
 
 const center = {
-  lat: 37.5665, // 서울 위도
-  lng: 126.978, // 서울 경도
+    lat: 37.5665, // 서울 위도
+    lng: 126.978, // 서울 경도
 };
 
 const MapComponent = () => {
-  return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_TH_API_KEY}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-        <Marker position={center} />
-      </GoogleMap>
-    </LoadScript>
-  );
+    return (
+        <LoadScript
+            googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_TH_API_KEY}
+        >
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={10}
+            >
+                <Marker position={center} />
+            </GoogleMap>
+        </LoadScript>
+    );
 };
 
 export default MapComponent;
